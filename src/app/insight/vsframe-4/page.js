fb5d2d27c6c482f23a2e16cc41476c6d4c636148
erramesh000 <erramesh000@gmail.com>
2026-05-18 22:20:49 +0530

    update project 4

--- src/app/insight/vsframe-4/page.js
+++ src/app/insight/vsframe-4/page.js
@@ -5,7 +5,7 @@ export default function Project() {
     <main className="max-w-6xl mx-auto bg-white rounded-2xl shadow-xl overflow-hidden mt-5">
 
       <div className="relative h-72 sm:h-96 md:h-[28rem] w-full group">
-        <Image src="/images/vsframe-12.jpg"
+        <Image src="/images/vsframe30.jpg"
           alt="VSFRAME"
           fill
           sizes="100vw"
@@ -90,6 +90,26 @@ export default function Project() {
                 </li>
               </ul>
             </div>
+          </div>
+        </div>
+        <div className="mt-12 pt-10 border-t border-gray-200">
+          <h2 className="text-3xl font-bold text-gray-900 mb-8">Project Gallery</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-12">
+            <div className="relative h-64 w-full rounded-lg overflow-hidden shadow-lg">
+              <Image src="/images/vsframe30.jpg" alt="vsframe15" fill className="w-full h-full object-cover hover:scale-105 transition-transform duration-300" />
+            </div>
+            <div className="relative h-64 w-full rounded-lg overflow-hidden shadow-lg">
+              <Image src="/images/vsframe31.jpg" alt="vsframe16" fill className="w-full h-full object-cover hover:scale-105 transition-transform duration-300" />
+            </div>
+            <div className="relative h-64 w-full rounded-lg overflow-hidden shadow-lg">
+              <Image src="/images/vsframe35.jpg" alt="vsframe17" fill className="w-full h-full object-cover hover:scale-105 transition-transform duration-300" />
+            </div>
+            <div className="relative h-64 w-full rounded-lg overflow-hidden shadow-lg">
+              <Image src="/images/vsframe36.jpg" alt="vsframe18" fill className="w-full h-full object-cover hover:scale-105 transition-transform duration-300" />
+            </div>
+
+
+
           </div>
         </div>
 
